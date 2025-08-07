7c5c3dd324ac839bd13ad78a2f139a537c4607d8
Jonathan Xina <john.xina.p88@gmail.com>
2025-08-07 16:13:33 +0700

    feat: implement DLP contract initialization with asset parameter
    - Updated the DLP contract to include an initializer function that accepts an asset address.
    - Integrated IERC20 from OpenZeppelin to ensure proper asset handling during initialization.

--- src/tokens/DLP.sol
+++ src/tokens/DLP.sol
@@ -1,7 +1,10 @@
 pragma solidity 0.8.20;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ERC4626Upgradeable } from "openzeppelin-contracts-upgradeable-5.4.0/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
 
 contract DLP is ERC4626Upgradeable {
-  constructor() ERC4626Upgradeable() {}
+  function initialize(address asset) public initializer {
+    __ERC4626_init(IERC20(asset));
+  }
 }
