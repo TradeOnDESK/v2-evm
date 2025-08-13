e030e3d1b000fb7baf2362baf7c31e847ea9fe48
Jonathan Xina <john.xina.p88@gmail.com>
2025-08-13 09:51:29 +0700

    fix: update remappings for OpenZeppelin contracts
    - Adjusted remapping paths for OpenZeppelin contracts to include the correct namespace.
    - Updated import statements in DLP contract to reflect the new remapping structure.

--- src/tokens/DLP.sol
+++ src/tokens/DLP.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.20;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { ERC4626Upgradeable } from "openzeppelin-contracts-upgradeable-5.4.0/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import { ERC4626Upgradeable } from "@openzeppelin-contracts-upgradeable-5.4.0/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
 
 contract DLP is ERC4626Upgradeable {
   function initialize(address asset) public initializer {
