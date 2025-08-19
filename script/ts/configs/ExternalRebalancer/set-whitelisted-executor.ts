feecfc2ee015991e913d21f13246264a0f2128ff
Jonathan Xina <john.xina.p88@gmail.com>
2025-08-19 15:22:55 +0700

    refactor: update ExternalRebalancer script to use factory pattern for contract instantiation

--- script/ts/configs/ExternalRebalancer/set-whitelisted-executor.ts
+++ script/ts/configs/ExternalRebalancer/set-whitelisted-executor.ts
@@ -2,7 +2,7 @@ import { loadConfig } from "../../utils/config";
 import { Command } from "commander";
 import signers from "../../entities/signers";
 import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
-import { ethers } from "hardhat";
+import { ExternalRebalancer__factory } from "../../../../typechain";
 
 async function main(chainId: number) {
   const config = loadConfig(chainId);
@@ -10,17 +10,16 @@ async function main(chainId: number) {
   const ownerWrapper = new OwnerWrapper(chainId, deployer);
 
   // The executor address to whitelist
-  const whitelistedExecutor = "0x6a5D2BF8ba767f7763cd342Cb62C5076f9924872"; // Update this address as needed
+  const whitelistedExecutor = "0x6a5D2BF8ba767f7763cd342Cb62C5076f9924872";
 
-  const externalRebalancer = await ethers.getContractFactory("ExternalRebalancer", deployer);
-  const externalRebalancerContract = externalRebalancer.attach(config.handlers.externalRebalancer);
+  const externalRebalancer = ExternalRebalancer__factory.connect(config.handlers.externalRebalancer, deployer);
 
   console.log(`[configs/ExternalRebalancer] Set Whitelisted Executor`);
   console.log(`[configs/ExternalRebalancer] Executor: ${whitelistedExecutor}`);
 
   await ownerWrapper.authExec(
-    externalRebalancerContract.address,
-    externalRebalancerContract.interface.encodeFunctionData("addWhitelistedExecutor", [whitelistedExecutor])
+    externalRebalancer.address,
+    externalRebalancer.interface.encodeFunctionData("addWhitelistedExecutor", [whitelistedExecutor])
   );
 
   console.log("[configs/ExternalRebalancer] Finished");
